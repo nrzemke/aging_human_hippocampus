06a3e42c3b081751b42a7d1e64c5a17b1991812f
nrzemke <65197527+nrzemke@users.noreply.github.com>
2025-07-08 18:05:01 -0700

    Create tf_motif_fimo_loop.sh

---
+++ 03_age_correlation/tf_motif_fimo_loop.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+motif_dir='/tscc/nfs/home/nzemke/renlab2/multiome/mop_m1_integration/human_atac/motif_epi_conservation/motif_files/JASPAR2022_CORE_vertebrates_non-redundant_pfms_meme/'
+
+counter=0
+for x in ${motif_dir}* ; do 
+    if [ $counter -lt 100 ]; then
+  motif=$(echo $x | sed "s=${motif_dir}==g");
+echo $motif
+   ((counter++))
+    else
+        break
+    fi
+
+fimo --o fimo_motif_out/${motif} ${motif_dir}${motif} union_peaks.fa
+
+awk 'BEGIN{FS=OFS="\t"}($9<0.05){print $3,$4,$5,$6}' fimo_motif_out/${motif}/fimo.tsv | awk 'BEGIN {OFS=FS="\t"} {gsub("[:-]", "\t", $1); print}' | awk 'BEGIN{FS=OFS="\t"}{print $1,$2+$4,$2+$5,$6}' > fimo_motif_out/${motif}/motif_q.05.bed
+
+done
